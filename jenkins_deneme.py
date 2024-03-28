eae2d49e66d7414f21bcf7e709e9a6a941525cc8
ozgunfurkan <ozgunfurkan2626@gmail.com>
2024-03-28 14:52:05 +0300

    Add files via upload

--- jenkins_deneme.py
+++ jenkins_deneme.py
@@ -1,1 +1,20 @@
-print("hello world")
+import os
+from flask import Flask, request, g
+
+print("jenkins api deneme başlıyor...")
+
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def helloworld():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run()
+
+
